7e52960036b82e68661cb3d6ff7f2a7b2b265099
belltree <c0a22087e5@edu.teu.ac.jp>
2023-10-31 13:59:07 +0900

    初期状態

---
+++ game.py
@@ -0,0 +1,23 @@
+import random
+import sys
+import time
+
+import pygame as pg
+
+
+WIDTH = 1600  # ゲームウィンドウの幅
+HEIGHT = 900  # ゲームウィンドウの高さ
+
+
+
+
+
+
+def main():
+    
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.wuit()
+    sys.exit()
