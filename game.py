d59cb61b36db7d905455a69b8e2d19ec1ab7b595
Gobou <c0a2210322@edu.teu.ac.jp>
2023-10-31 18:12:13 +0900

    GameOver

--- game.py
+++ game.py
@@ -6,18 +6,29 @@ import pygame as pg
 WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
 
+class GameOver:
+    # プレイヤーと敵が接触したら終わり（現時点は自己申告）
+    def touch_enemy(screen):
+        font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 80)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 0
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                txt = font.render("GameOver", True, (255, 255, 255))
+                screen.blit(txt, [250, 250])
+        pg.display.update()
 
-
-
+    # 時間切れで終わり
+    # 範囲外にキャラクターが出て終わり
 
 
 def main():
     pg.display.set_caption("タイトル")
     screen = pg.display.set_mode((800,600))
     back = pg.image.load("ex05/haikei.png")
+    screen.blit(back,[0,0])
     while True:
-        screen.blit(back,[0,0])
-        pg.display.update()
+        GameOver.touch_enemy(screen)
 
 if __name__ == "__main__":
     pg.init()
