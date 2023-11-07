f97bb232720e34839ffeda7b57e6bb9a6ba85098
asahidayo <c0a2203187@edu.teu.ac.jp>
2023-11-07 14:09:51 +0900

    敵mob追加

--- game.py
+++ game.py
@@ -8,21 +8,40 @@ HEIGHT = 900  # ゲームウィンドウの高さ
 
 
 class Tekimod:
+    
     def __init__(self):
         #キラーの初期設定
-        self.kira_img=pg.image.load("ex05/fig/kira.jpg")
-        self.kira_img=pg.transform.rotozoom(self.kira_img,0,0.1)
+        
+        self.kira_img=pg.image.load("ex05/fig/kira1.1.png")
+        self.kira_img=pg.transform.rotozoom(self.kira_img,0,0.2)
         self.kira_rct=self.kira_img.get_rect()
-        self.kira_rct.center=[1600,600]
-        self.kira_x=-1
+        self.kira_rct.center=[1600,400]
+        self.kira_x=-2
         self.kira_y=0
-        #ボム兵
-        self.bomu_img=pg.image.load("ex05/fig/bom.jpg")
-        self.bomu_img=pg.transform.rotozoom(self.bomu_img,0,0.3)
+        #ボム兵  self.image = tools.load_image("data", "rabipple.png", -1)
+
+        self.bomu_img=pg.image.load("ex05/fig/bomu1.1.png")
+        self.bomu_img=pg.transform.rotozoom(self.bomu_img,0,2)
         self.bomu_rct=self.bomu_img.get_rect()
         self.bomu_rct.center=[800,0]
         self.bomu_x=0
-        self.bomu_y=+1
+        self.bomu_y=+3
+
+        #クリボー
+        self.kuribo_img=pg.image.load("ex05/fig/kuribo1.1.png")
+        self.kuribo_img=pg.transform.rotozoom(self.kuribo_img,0,2)
+        self.kuribo_rct=self.kuribo_img.get_rect()
+        self.kuribo_rct.center=[1500,600]
+        self.kuribo_x=-1
+        self.kuribo_y=0
+
+        #パックン
+        self.pakkun_img=pg.image.load("ex05/fig/pakkun1.1.png")
+        self.pakkun_img=pg.transform.rotozoom(self.pakkun_img,0,2)
+        self.pakkun_rct=self.pakkun_img.get_rect()
+        self.pakkun_rct.center=[1200,600]
+        
+
 
     def update(self, screen: pg.Surface):
         #キラー
@@ -31,6 +50,11 @@ class Tekimod:
         #ボムー
         self.bomu_rct.move_ip(self.bomu_x,self.bomu_y)
         screen.blit(self.bomu_img,self.bomu_rct)
+        #クリボー
+        self.kuribo_rct.move_ip(self.kuribo_x,self.kuribo_y)
+        screen.blit(self.kuribo_img,self.kuribo_rct)
+        #パックン
+        screen.blit(self.pakkun_img,self.pakkun_rct)
 
 
 
@@ -50,7 +74,8 @@ def main():
         #         return
        
         screen.blit(back,[0,0])
-        teki.update(screen)
+        teki.update(screen)#敵mobを追加
+        
         
     
         
@@ -61,5 +86,10 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
+
+#mobの背景をなくす
+#画面外に出たら消す
+#複数mobが生成されるようにする。
     
 
